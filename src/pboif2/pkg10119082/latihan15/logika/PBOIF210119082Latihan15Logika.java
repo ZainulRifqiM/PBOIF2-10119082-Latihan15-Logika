47803e0ebe72f12bfe4aa2b6158954cf2daf4058
Zainul Rifqi <Zainul Rifqi@LAPTOP-O1E2M14H>
2020-10-17 17:10:53 +0700

    PBOIF2-10119082-Latihan15-Logika

---
+++ src/pboif2/pkg10119082/latihan15/logika/PBOIF210119082Latihan15Logika.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119082.latihan15.logika;
+
+/**
+ *
+ * @author Zainul Rifqi
+ * NAMA     : Zainul Rifqi Muwaffaq
+ * KELAS    : IF2
+ * NIM      : 10119082
+ * Deskripsi Program : Logika
+ */
+public class PBOIF210119082Latihan15Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " + !a);
+        System.out.println("!(a && b) = " + !(a && b));
+    }
+    
+}
